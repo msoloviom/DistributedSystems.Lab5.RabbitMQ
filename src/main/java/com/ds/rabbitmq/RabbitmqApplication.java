ffbed2acc04951c6d3aab3ad0a6baa58d30a050e
msoloviom <marinaby@i.ua>
2018-05-06 03:51:01 +0300

    add basic message queue classes and its configs

---
+++ src/main/java/com/ds/rabbitmq/RabbitmqApplication.java
@@ -0,0 +1,7 @@
+package com.ds.rabbitmq;
+
+public class RabbitmqApplication {
+
+	public static void main(String[] args) {
+	}
+}
---
+++ src/main/java/com/ds/rabbitmq/work/queues/Worker.java
@@ -0,0 +1,64 @@
+package com.ds.rabbitmq.work.queues;
+
+import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+import java.io.IOException;
+
+public class Worker {
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
+
+        /* Two things are required to make sure that messages aren't lost:
+         * we need to mark both the queue and messages as durable.
+         */
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // Fair dispatch
+        channel.basicQos(1);
+
+        final Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+
+                System.out.println(" [x] Received '" + message + "'");
+                try {
+                    doWork(message);
+                } finally {
+                    System.out.println(" [x] Done");
+                    /*
+                     * There aren't any message timeouts; RabbitMQ will redeliver the message
+                     * when the consumer dies.
+                     * It's fine even if processing a message takes a very, very long time.
+                     */
+                    channel.basicAck(envelope.getDeliveryTag(), false);
+                }
+            }
+        };
+        channel.basicConsume(TASK_QUEUE_NAME, false, consumer);
+    }
+
+    private static void doWork(String task) {
+        for (char ch : task.toCharArray()) {
+            if (ch == '.') {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException _ignored) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
